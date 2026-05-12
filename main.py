8bcf98665ef8c102f1d186bbf7b09887f6e2b221
Gregory Rodriguez <glrd4726ml@gmail.com>
2026-05-12 20:09:14 -0300

    Documentando funciones

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from src.funciones import inicio
 
 if __name__ == "__main__":
+    """Punto de entrada principal del programa
+Llama a la función de inicio para comenzar la ejecución del programa de lista de alumnos
+    """
     inicio()
--- src/funciones.py
+++ src/funciones.py
@@ -9,10 +9,23 @@ editado:str=""
 indice:int=0
 
 
+"""
+Funciones para el programa de lista de alumnos
+Creado por: Gregory Rodriguez
+"""
 def borrarPantalla():
+    """
+    Función para limpiar la pantalla de la consola
+    """
     os.system("cls") if os.name == "nt" else os.system("clear")
 
 def agregarAlumno():
+    """
+    Función para agregar un alumno a la lista
+    
+    Verifica si el alumno ya existe antes de agregarlo
+
+    """
     borrarPantalla()
     print("\nAgregar alumno\n")
     alumno=input("\nIndique nombre del alumno a guardar\n").capitalize()
@@ -23,6 +36,11 @@ def agregarAlumno():
         input("\nAlumno agregado\nPresiona una tecla para continuar . . .\n ")
 
 def mostrarAlumnos():
+    """
+    Función para mostrar la lista de alumnos guardados
+    Verifica si hay alumnos guardados antes de mostrar la lista
+    
+    """
     borrarPantalla()
     if len(alumnos) == 0:
         input("\nNo hay alumnos guardados\nPresione una tecla para continuar . . .\n")
@@ -35,6 +53,11 @@ def mostrarAlumnos():
     input("\nPulse alguna tecla para salir . . .\n")
 
 def buscarAlumno():
+    """
+    Función para buscar un alumno en la lista
+    Verifica si el alumno existe en la lista antes de mostrar el resultado
+    
+    """
     borrarPantalla()
     print("\nBuscar alumno\n")
     alumno=input("\nIndique el alumno a buscar\n").capitalize()
@@ -44,6 +67,10 @@ def buscarAlumno():
         input("\nAlumno no encontrado\nPresione una tecla para continuar . . . .\n")
 
 def actualizarAlumno():
+    """
+    Función para actualizar el nombre de un alumno en la lista
+    Verifica si el alumno existe en la lista antes de permitir la actualización
+    """
     borrarPantalla()
     print("\nActualizar alumno\n")
     numeracion=1
@@ -62,6 +89,10 @@ def actualizarAlumno():
 
 
 def eliminarAlumno():
+    """
+    Función para eliminar un alumno de la lista
+    Verifica si el alumno existe en la lista antes de permitir la eliminación
+    """
     borrarPantalla()
     print("\nEliminar alumno\n")
     numeracion=1
@@ -78,6 +109,10 @@ def eliminarAlumno():
 
 
 def salir():
+    """
+    Función para salir del programa
+    Limpia la pantalla y muestra un mensaje de despedida antes de cerrar el programa
+    """
     borrarPantalla()
     input("\nSaliendo . . . .\nPresione una tecla para continuar. . . . .\n")
     borrarPantalla()
@@ -87,15 +122,23 @@ def salir():
     borrarPantalla()
 
 def menu():
+   """Función para mostrar el menú de opciones al usuario y obtener su selección
+Limpia la pantalla antes de mostrar el menú y devuelve la opción seleccionada por el usuario como un entero
+
+"""
    borrarPantalla()
    opcion = int( input("**********Menú*************\n1.- Agregar alumno\n2.- Mostrar lista\n3.- Buscar articulo\n4.- Actualizar articulo\n5.- Eliminar articulo\n6.- Salir\nIndique su opción:\n"))
    return opcion
 
 def diferente():
-    borrarPantalla()
+    """Función para manejar opciones no válidas en el menú
+Muestra un mensaje de error al usuario indicando que la opción seleccionada no es válida y espera a que el usuario presione una tecla para continuar"""
     input("\nOpción no existente\nPresione una tecla para continuar . . .\n")
 
 def inicio():
+    """Función principal para iniciar el programa de lista de alumnos
+Muestra un mensaje de bienvenida al usuario y luego entra en un bucle que muestra el menú de opciones y maneja la selección del usuario hasta que el usuario elige salir del programa
+"""
     print("Bienvenido a lista de alumnos\n")
 
     opcion:int=0
