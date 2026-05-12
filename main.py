b3b0ea017b5cfcb3bba22fc4c29e97b5ea582c3d
Gregory Rodriguez <glrd4726ml@gmail.com>
2026-05-12 08:14:55 -0300

    Fix

--- main.py
+++ main.py
@@ -1,34 +1,4 @@
-from src.funciones import actualizarAlumno, agregarAlumno, buscarAlumno, eliminarAlumno, menu, mostrarAlumnos, salir, diferente
-
-def inicio():
-    print("Bienvenido a lista de alumnos\n")
-
-    opcion:int=0
-
-    while opcion != 6:
-        
-        try:
-            opcion = menu()
-
-            match opcion:
-                case 1:
-                    agregarAlumno()
-                case 2:
-                    mostrarAlumnos()
-                case 3:
-                    buscarAlumno()
-                case 4:
-                    actualizarAlumno()
-                case 5:
-                    eliminarAlumno()
-                case 6:
-                    salir()
-                case _:
-                    diferente()
-        except ValueError :
-            input("\nDebe introducir un dato valido por teclado\nPresione una tecla para continuar . . .\n") 
-        except Exception as e:
-            input(f"\nOcurrió un error: {e}\nPresione una tecla para continuar . . .\n")
+from src.funciones import inicio
 
 if __name__ == "__main__":
     inicio()
--- src/funciones.py
+++ src/funciones.py
@@ -79,7 +79,7 @@ def eliminarAlumno():
 
 def salir():
     borrarPantalla()
-    input("\nSaliendo . . . .\nPresione una tecla para continuar")
+    input("\nSaliendo . . . .\nPresione una tecla para continuar. . . . .\n")
     borrarPantalla()
     print("Gracias por usar el programa\n")
     print("Creado por: Gregory Rodriguez\n")
@@ -94,5 +94,35 @@ def menu():
 def diferente():
     borrarPantalla()
     input("\nOpción no existente\nPresione una tecla para continuar . . .\n")
+
+def inicio():
+    print("Bienvenido a lista de alumnos\n")
+
+    opcion:int=0
+
+    while opcion != 6:
+        
+        try:
+            opcion = menu()
+
+            match opcion:
+                case 1:
+                    agregarAlumno()
+                case 2:
+                    mostrarAlumnos()
+                case 3:
+                    buscarAlumno()
+                case 4:
+                    actualizarAlumno()
+                case 5:
+                    eliminarAlumno()
+                case 6:
+                    salir()
+                case _:
+                    diferente()
+        except ValueError :
+            input("\nDebe introducir un dato valido por teclado\nPresione una tecla para continuar . . .\n") 
+        except Exception as e:
+            input(f"\nOcurrió un error: {e}\nPresione una tecla para continuar . . .\n")
 
 
